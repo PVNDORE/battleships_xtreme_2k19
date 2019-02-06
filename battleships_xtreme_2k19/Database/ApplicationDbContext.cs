f27fb34e0d6a5430eab065ae38543e42b9743087
matteomoisan22 <matteodev22@gmail.com>
2019-02-06 15:19:27 +0100

    installation Entity + création de la class AppDbContext

---
+++ battleships_xtreme_2k19/Database/ApplicationDbContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace battleships_xtreme_2k19.Database
+{
+    public class ApplicationDbContext
+    {
+
+        #region StaticVariables
+        #endregion
+
+        #region Constants
+        #endregion
+
+        #region Variables
+        #endregion
+
+        #region Attributs
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public ApplicationDbContext()
+        {
+
+        }
+        #endregion
+
+        #region StaticFunctions
+        #endregion
+
+        #region Functions
+        #endregion
+
+        #region Events
+        #endregion
+
+
+    }
+}
